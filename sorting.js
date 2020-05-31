e31b1e0eca357c369d5084dfadc7b6beb5b1600e
almonasterio <a.monasterio.a@gmail.com>
2020-05-31 12:02:04 -0500

    sorting algorithms insertion and selection

--- sorting.js
+++ sorting.js
@@ -13,3 +13,37 @@ function bubbleSort(arr) {
     }
     return arr
 }
+
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIdx = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        if (i != minIdx) {
+            let temp = arr[i]
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
+    return arr
+}
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currentVal = arr[i];
+        for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            if (currentVal < arr[j]) {
+                let temp = arr[j + 1]
+                arr[j + 1] = arr[j]
+                arr[j] = temp
+            }
+
+        }
+
+    }
+    return arr
+}
