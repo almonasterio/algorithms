f68fa280234045ee3d2ad4388562e538b9387eb3
almonasterio <a.monasterio.a@gmail.com>
2020-06-22 12:17:45 -0500

    breadth First Search and Deep First Search implemented

--- data_structures/BinarySearchTrees.js
+++ data_structures/BinarySearchTrees.js
@@ -50,6 +50,30 @@ class BinarySearchTree {
          }
          return false;
      }
+     BSF(){
+         let data=[];
+         let queue=[];
+         let node=this.root
+         queue.push(node);
+         while(queue.length){
+             node=queue.shift();
+             data.push(node.val);
+             if (node.left) queue.push(node.left);
+             if (node.right) queue.push(node.right);
+         }
+         return data;
+     }
+      DFS() {
+          let data = [];
+          let current = this.root;
+          let helperFunction = (node) => {
+                data.push(node.val);
+                if (node.left) helperFunction(node.left);
+                if (node.right) helperFunction(node.right);
+
+          }
+          return data;
+      }
 
 }
 
