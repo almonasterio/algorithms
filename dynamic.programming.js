c1f662dc5f4cfd2850809ec1a35d9e2e148752fd
almonasterio <a.monasterio.a@gmail.com>
2020-06-29 15:26:59 -0500

    implemented dynamic programming exaample

---
+++ dynamic.programming.js
@@ -0,0 +1,16 @@
+function fib_nemo(n,nemo=[]){
+    if (nemo[n] !== undefined) return nemo[n];
+    if (n<=2) return 1;
+    var res=fib(n-1,nemo) +fib(n-2,nemo);
+    nemo[n]=res;
+    return res;
+}
+
+function fib_table(n){
+    if (n <= 2) return 1;
+    var fibNums = [0,1,1];
+    for(var i=3;i<=n;i++){
+        fibNums[i]= fibNums[i - 1] + fibNums[i - 2];
+    }
+    return fibNums[n];
+}
